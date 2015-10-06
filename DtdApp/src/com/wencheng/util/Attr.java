858db9f4525f50f562b18cec8e21c94f3c0bdce2
wencheng <wencheng256@gmail.com>
2015-10-06 14:46:05 +0800

    create

---
+++ DtdApp/src/com/wencheng/util/Attr.java
@@ -0,0 +1,24 @@
+package com.wencheng.util;
+
+public class Attr {
+	
+	private String name;
+	private boolean required;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isRequired() {
+		return required;
+	}
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+	@Override
+	public String toString(){
+		return "name:"+name+" required?:"+required;
+	}
+}
---
+++ DtdApp/src/com/wencheng/util/AttrList.java
@@ -0,0 +1,22 @@
+package com.wencheng.util;
+
+import java.util.List;
+
+public class AttrList {
+	
+	private String parent;
+	private List<Attr> list;
+	
+	public String getParent() {
+		return parent;
+	}
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+	public List<Attr> getList() {
+		return list;
+	}
+	public void setList(List<Attr> list) {
+		this.list = list;
+	}
+}
